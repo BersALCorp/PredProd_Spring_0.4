0885bb741075ea06008148081acb7a441366bf81
Bers-AL <Bers666@gmail.com>
2023-08-21 00:47:16 +0300

    done. added several methods

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -12,51 +12,51 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-    @Autowired
-    private SessionFactory sessionFactory;
+   @Autowired
+   private SessionFactory sessionFactory;
 
-    @Override
-    public void add(User user) {
-        sessionFactory.getCurrentSession().save(user);
-    }
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
 
-    @Override
-    public void update(User user) {
-        sessionFactory.getCurrentSession().saveOrUpdate(user);
-    }
+   @Override
+   public void update(User user) {
+      sessionFactory.getCurrentSession().saveOrUpdate(user);
+   }
 
-    @Override
-    public void delete(User user) {
-        sessionFactory.getCurrentSession().delete(user);
-    }
+   @Override
+   public void delete(User user) {
+      sessionFactory.getCurrentSession().delete(user);
+   }
 
-    @Override
-    public User getByUserId(long id) {
-        return sessionFactory.getCurrentSession().get(User.class, id);
-    }
+   @Override
+   public User getByUserId(long id) {
+      return sessionFactory.getCurrentSession().get(User.class, id);
+   }
 
-    @Override
-    @SuppressWarnings("unchecked")
-    public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
-        return query.getResultList();
-    }
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
 
-    @Override
-    public List<User> getUsersByCarModelAndSeries(String model, int series) {
-        String hql = "SELECT DISTINCT u FROM User u INNER JOIN Car c ON u.id = c.user.id WHERE c.model = :model AND c.series = :series";
-        Query<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
-        query.setParameter("model", model);
-        query.setParameter("series", series);
-        return query.getResultList();
-    }
+   @Override
+   public List<User> getUsersByCarModelAndSeries(String model, int series) {
+      String hql = "SELECT DISTINCT u FROM User u INNER JOIN Car c ON u.id = c.user.id WHERE c.model = :model AND c.series = :series";
+      Query<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      return query.getResultList();
+   }
 
-    public User getFirstUserByCarModelAndSeries(String model, int series, String direction) {
-        String hql = "SELECT u FROM User u INNER JOIN Car c ON u.id = c.user.id WHERE c.model = :model AND c.series = :series ORDER BY u.id " + direction;
-        Query<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
-        query.setParameter("model", model);
-        query.setParameter("series", series);
-        query.setMaxResults(1);
-        return query.uniqueResult();
-    }
+   public User getFirstUserByCarModelAndSeries(String model, int series, String direction) {
+      String hql = "SELECT u FROM User u INNER JOIN Car c ON u.id = c.user.id WHERE c.model = :model AND c.series = :series ORDER BY u.id " + direction;
+      Query<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      query.setMaxResults(1);
+      return query.uniqueResult();
+   }
 }
